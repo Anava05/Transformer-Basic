d7035c3b818016df9b24c17688d48c9498dd28c3
Anava05 <anava9799308@gmail.com>
2025-08-07 21:23:29 +0530

    commit

---
+++ transformer_GQA.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import math
+
+class attention(nn.Module):
+    def __init__(self,mask=None, dropout=None,dim=512):
+        super(attention, self).__init__()
+        self.mask = mask
+        self.dropout = dropout
+        self.softmax = nn.Softmax(dim=-1)
+        self.dim = dim    
+
+    def forward(self, q, k, v,n):
+        scores = torch.matmul(q, k.transpose(-2, -1))
+        if self.mask is not None:
+            scores = scores.masked_fill(self.mask, -float('inf'))
+        scores = scores / torch.sqrt(torch.tensor(self.dim, dtype=scores.dtype))
+        #we have also extracted value of sequence length because if we want to include positional bias right here in the attention block itself then we must be able to handle it 
+        p_attn = self.softmax(scores)
+        if self.dropout is not None:
+            p_attn = self.dropout(p_attn)
+        return torch.matmul(p_attn, v)
+
+class normalize(nn.Module):
+    def __init__(self,d_model):
+        super(normalize, self).__init__()
+        self.norm = nn.LayerNorm(d_model)
+    def forward(self, x):
+        return self.norm(x)
+
+
+class MHA(nn.Module):
+    def __init__(self,d_model,heads,Q_dim,dropout=None,GQA = None):
+        super(MHA, self).__init__()
+        self.d_model = d_model
+        self.heads = heads
+        self.q = nn.LazyLinear(Q_dim)
+        self.k = nn.LazyLinear(Q_dim)
+        self.v = nn.LazyLinear(Q_dim)
+        self.dropout = nn.Dropout(dropout)
+        if Q_dim % heads != 0:
+            raise ValueError("Q_dim must be divisible by heads")
+        self.head_dim = Q_dim // heads
+        self.attn = attention(mask=None, dropout=self.dropout,dim = self.head_dim)
+        self.out = nn.LazyLinear(d_model)
+        self.kv_cache = None  # Initialize the KV cache
+        self.GQA = GQA
+
+    def forward(self, embeddings, use_cache=False, past_kv=None):
+        GQA = self.GQA
+        n = embeddings.size(1)
+        q = self.q(embeddings)
+        k = self.k(embeddings)
+        v = self.v(embeddings)
+        q = q.view(q.size(0), -1, self.heads, self.head_dim).transpose(1, 2)
+        k = k.view(k.size(0), -1, self.heads, self.head_dim).transpose(1, 2)
+        v = v.view(v.size(0), -1, self.heads, self.head_dim).transpose(1, 2)
+        #GQA is like an downsampling and then storing for saving memory while getting similar or close reconstruction of the original kv_cache
+        if GQA is not None:
+            k = k[:, :, ::GQA, :]
+            v = v[:, :, ::GQA, :]
+        if use_cache:
+            if past_kv is not None: #If we have past_kv , then we will add the new key,values to the cache
+                k = torch.cat([past_kv[0], k], dim=2) #Concatenate along sequence length dimension
+                v = torch.cat([past_kv[1], v], dim=2)
+        #reversing the effects of GQA by interpolation 
+        expanded_k = torch.zeros(k.shape[0], k.shape[1], n, k.shape[3]).to(k.device)
+        expanded_v = torch.zeros(v.shape[0], v.shape[1], n, v.shape[3]).to(v.device)
+        expanded_k[:, :, ::GQA, :] = k
+        expanded_v[:, :, ::GQA, :] = v
+        expanded_k[:, :, 1::GQA, :] = expanded_k[:, :, ::GQA, :].repeat(1, 1, GQA-1, 1)
+        expanded_v[:, :, 1::GQA, :] = expanded_v[:, :, ::GQA, :].repeat(1, 1, GQA-1, 1)
+        # we pass expanded_k and expanded_v instead of k and v becaus e they are the reconstructions of real K and V from our downsampled K and V
+        attn_output = self.attn(q, expanded_k, expanded_v,n)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(attn_output.size(0), -1, self.heads * self.head_dim)
+        attn_output = self.out(attn_output)
+        
+        #Update KV Cache
+        if use_cache:
+            self.kv_cache = (k, v) # This handles both cases when the KV_cache is None or if not None.
+        return attn_output
+
+
+class transformers(nn.Module):
+    def __init__(self,d_model,heads,Q_dim,dropout=None):
+        super(transformers, self).__init__()
+        self.mha = MHA(d_model=d_model,heads=heads,Q_dim=Q_dim,dropout=dropout)
+        self.norm1 = normalize(d_model)
+        self.norm2 = normalize(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.ffnn = nn.Sequential(
+            nn.LazyLinear(4*d_model),
+            nn.ReLU(),
+            nn.LazyLinear(d_model)
+        )
+
+    def forward(self, embeddings, use_cache=False, past_kv=None):
+        #Pass KV cache parameters to MHA 
+        x = self.mha(self.norm1(embeddings), use_cache, past_kv)
+        x = x + self.dropout1(x)
+        out = self.ffnn(self.norm2(x))
+        x = x + self.dropout2(out)
+        return x
